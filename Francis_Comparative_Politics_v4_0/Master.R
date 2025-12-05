7452cd2b5f3fb9f133d337e4cd73b38f8720e7c9
joefrancis505 <joefrancis505@gmail.com>
2025-12-05 22:49:27 +0000

    delete zip
    deleted a zip file

--- Francis_Comparative_Politics_v4_0/Master.R
+++
@@ -1,9 +0,0 @@
-# Set the working directory to the script’s location
-setwd(getSrcDirectory(function(dummy) {dummy}))
-
-# Clear console
-cat("\014")
-
-source("Datasets.R")
-source("Replication.R")
-source("Gap_plot.R")
